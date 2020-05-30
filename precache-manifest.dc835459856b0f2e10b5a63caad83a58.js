b7fb96b9c46d281db3c17eca9d73dfda1e769a0f
Visal Rajapakse <visalrajapakse@gmail.com>
2020-05-31 02:31:11 +0530

    Updates

---
+++ precache-manifest.dc835459856b0f2e10b5a63caad83a58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9141aef8efe5bbc2d29090757b8775ea",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "62ece101437d89012269",
+    "url": "/Personal-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24acc32fdd904752fb07",
+    "url": "/Personal-Portfolio/static/js/2.8d56a7c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Portfolio/static/js/2.8d56a7c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ece101437d89012269",
+    "url": "/Personal-Portfolio/static/js/main.51694dd9.chunk.js"
+  },
+  {
+    "revision": "ade82194d1cb4bbae8c2",
+    "url": "/Personal-Portfolio/static/js/runtime-main.9fd70a07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Personal-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
